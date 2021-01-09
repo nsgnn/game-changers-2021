24f327e8267a8e8aee0c7bd48ac41797ef3493f5
Nathan Gazey <nathan.gazey@beast-code.com>
2021-01-09 12:08:22 -0600

    added dynamic naming and such

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,9 @@
       </div>
     </nav>
     <DefaultLayout :grid-size="3">
-      <Picture slot="1" :image-source="dogPicURL" description=""/>
-      <TextEntry class="blue" slot="2"> This is a dog.</TextEntry>
-      <TextEntry class="blue" slot="3"> This is a dog.</TextEntry>
-      <TextEntry class="blue" slot="4"> This is a dog.</TextEntry>
-      <TextEntry class="blue" slot="5"> This is a dog.</TextEntry>
-      <TextEntry class="blue" slot="6"> This is a dog.</TextEntry>
+      <Picture slot="1" :image-source="this.currentDogInfo['url']!=null? this.currentDogInfo['url'] : ''" description=""/>
+      <TextEntry slot="2"> {{this.dogDescription}}</TextEntry>
+
     </DefaultLayout>
   </div>
 </template>
@@ -39,29 +36,28 @@ export default {
     BarGraph,
     Blank
   },
-  props:{
-    pageName: {
-      type: String,
-      default: "Page Name"
-    }
-  },
   data: function (){
     return {
       dogAPIKey: '06a42eea-e1d4-4b40-946d-fb2464169421',
-      dogPicURL: null
-
+      currentDogInfo: {"breeds":[{"weight":{"imperial":"45 - 55","metric":"20 - 25"},"height":{"imperial":"20 - 22","metric":"51 - 56"},"id":32,"name":"Bearded Collie","bred_for":"Sheep herding","breed_group":"Herding","life_span":"12 - 14 years","temperament":"Self-confidence, Hardy, Lively, Alert, Intelligent, Active","reference_image_id":"A09F4c1qP"}],"id":"I3E0R2tuV","url":"https://cdn2.thedogapi.com/images/I3E0R2tuV.jpg","width":1080,"height":1080},
+      pageName: '',
     }
   },
   async mounted() {
-    this.randomDogPicture();
+    await this.getRandomDogInfo();
+    this.updatePageInfo();
   },
   methods:{
     async getFromURL(url, key) {
       return axios.get(url, {headers: {'X-api-key': key}});
     },
-    async randomDogPicture(){
+    async getRandomDogInfo(){
       await this.getFromURL("https://api.thedogapi.com/v1/images/search", this.dogAPIKey)
-          .then((response) => this.dogPicURL = response.data[0].url);
+          .then((response) => this.currentDogInfo = response.data[0]);
+    },
+    updatePageInfo(){
+      this.pageName = this.currentDogInfo.breeds[0].name;
+      // this.
     }
   }
 }
