5d151185cdd7b4338d074539523c895233b9a705
Nathan Gazey <nategg@pm.me>
2021-01-09 12:28:56 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <nav>
       <div class="nav-wrapper blue">
-        <a class="brand-logo center no-select">{{ this.pageName }}</a>
+        <a class="brand-logo  center no-select">{{ this.pageName }}</a>
         <ul class="right hide-on-med-and-down">
           <li class="waves-effect waves-light"><a onclick="location.reload()"><i class="material-icons no-select">refresh</i></a></li>
         </ul>
       </div>
     </nav>
-    <DefaultLayout :grid-size="3">
+    <DefaultLayout :grid-size="2">
       <Picture slot="1" :image-source="this.currentDogInfo['url']!=null? this.currentDogInfo['url'] : ''" description=""/>
       <TextEntry slot="2"> {{this.dogDescription}}</TextEntry>
 
@@ -39,7 +39,7 @@ export default {
   data: function (){
     return {
       dogAPIKey: '06a42eea-e1d4-4b40-946d-fb2464169421',
-      currentDogInfo: {"breeds":[{"weight":{"imperial":"45 - 55","metric":"20 - 25"},"height":{"imperial":"20 - 22","metric":"51 - 56"},"id":32,"name":"Bearded Collie","bred_for":"Sheep herding","breed_group":"Herding","life_span":"12 - 14 years","temperament":"Self-confidence, Hardy, Lively, Alert, Intelligent, Active","reference_image_id":"A09F4c1qP"}],"id":"I3E0R2tuV","url":"https://cdn2.thedogapi.com/images/I3E0R2tuV.jpg","width":1080,"height":1080},
+      currentDogInfo: {"breeds":[{"weight":{"imperial":"45 - 55","metric":"20 - 25"},"height":{"imperial":"20 - 22","metric":"51 - 56"},"id":32,"name":"","bred_for":"Sheep herding","breed_group":"Herding","life_span":"12 - 14 years","temperament":"Self-confidence, Hardy, Lively, Alert, Intelligent, Active","reference_image_id":"A09F4c1qP"}],"id":"I3E0R2tuV","url":"","width":1080,"height":1080},
       pageName: '',
     }
   },
@@ -56,8 +56,13 @@ export default {
           .then((response) => this.currentDogInfo = response.data[0]);
     },
     updatePageInfo(){
-      this.pageName = this.currentDogInfo.breeds[0].name;
+      this.pageName = this.currentDogInfo.breeds[0].name!== undefined? this.currentDogInfo.breeds[0].name : 'Dog Info';
       // this.
+    }
+  },
+  computed:{
+    dogDescription: function() {
+      return `The dog shown is a ${this.currentDogInfo.breeds[0].name}. People love these dogs, because they are ${this.currentDogInfo.breeds[0].temperament.toLowerCase()}. They usually live ${this.currentDogInfo.breeds[0].life_span} years.`;
     }
   }
 }
